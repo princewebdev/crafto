89ec398f48a3f8ac5b6f066cbfc0d29b7725ae3a
Shariar Mahmud Prince <81061039+princewebdev@users.noreply.github.com>
2024-12-09 13:12:44 +0600

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,3 @@
-// atropos js here
 document.addEventListener('DOMContentLoaded', function () {
   const feature = Atropos({
     el: '.feature-atropos',
@@ -7,20 +6,46 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 });
 
-// owl carusel js here
-$('.owl-carousel').owlCarousel({
-  loop:true,
-  margin:10,
-  nav:true,
-  responsive:{
-      0:{
-          items:1
-      },
-      600:{
-          items:3
-      },
-      1000:{
-          items:5
-      }
+// custom js for testimonail button
+document.addEventListener('DOMContentLoaded', function () {
+  let testiBoxes = document.querySelectorAll(".testimonial-box");
+  let testiBtn = document.querySelector(".testi-btn");
+  let allShown = false; // Track whether all boxes are shown
+
+  // Function to show limited boxes based on screen size
+  function showLimitedBoxes() {
+      const width = window.innerWidth;
+
+      testiBoxes.forEach((box, index) => {
+          if (width >= 1200 && index < 8) { 
+              box.style.display = "block"; 
+          } else if (width >= 768 && width < 1200 && index < 5) { 
+              box.style.display = "block"; 
+          } else if (width < 768 && index < 3) {
+              box.style.display = "block"; 
+          } else {
+              box.style.display = "none"; 
+          }
+      });
   }
-})
+
+  showLimitedBoxes();
+
+  testiBtn.addEventListener("click", () => {
+      if (allShown) {
+          showLimitedBoxes();
+          allShown = false;
+          testiBtn.style.display= "none"
+      } else {
+          // Show all boxes
+          testiBoxes.forEach(box => {
+              box.style.display = "block";
+            testiBtn.style.display= "none"
+
+          });
+          allShown = true;
+      }
+  });
+
+  window.addEventListener('resize', showLimitedBoxes);
+});
